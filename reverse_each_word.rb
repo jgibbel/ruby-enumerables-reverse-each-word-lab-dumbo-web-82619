50af1b840b115b76836d6fa69c0098fd28d1c447
James Gibbel <jamesgibbel@gmail.com>
2019-08-25 23:27:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(string)
+def reverse_each_word_no(string)
   new = string.split(/ /)
   ugh = []
   for x in 0..new.length-1 do
@@ -9,6 +9,12 @@ def reverse_each_word(string)
   p new
   ugh = ugh.join(' ')
   return ugh
+end
+
+    
+def reverse_each_word(string)
+    colarr = string.split(" ").collect{ |word| word.reverse}
+    colarr.join(" ")
 end
 
 reverse_each_word('hi hello how are you')
